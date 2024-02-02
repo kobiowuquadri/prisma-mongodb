3d4380e2bbb58fa8a6e77e8c9397c2abb7e19270
kobiowuquadri <kobiowuq@gmail.com>
2024-02-02 08:46:06 -0600

    add logic to login user

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,9 @@
 const prisma = require('../prisma/index')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// period
+const period = 1000 * 60 * 60 * 24 * 3
 
 // register
 
@@ -39,7 +43,37 @@ const SignUp = async (req, res) => {
   }
 }
 
-const SignIn = () => {}
+const SignIn = async (req, res) => {
+  try {
+    const {email, password} = req.body
+    if(!email || !password){
+      return res.status(400).json({success: false, message: "Please provide the fields."})
+    }
+    // validate email
+    const isEmail = await prisma.user.findUnique({where: {email}})
+    if(!isEmail){
+      return res.status(400).json({success: false, message: "Invalid email."})
+    }
+    // compare passwords
+    const isPassword = await bcrypt.compare(password, isEmail.password)
+    if(!isPassword){
+     return res.status(400).json({success: false, message: "Invalid password."})
+    }
+    // sign in user
+    jwt.sign({id: isEmail.id}, process.env.SECRECT, {expiresIn: "1d"}, async (err, token) => {
+      if(err){
+        throw new Error(err)
+      }
+      res.status(200).cookie('userId', isEmail.id, {maxAge: period ,httpOnly:true})
+      res.status(200).json({success: true, message:"User Logged in successfully", isEmail, token})
+    })
+
+  }
+  catch(err){
+    console.log(err.message)
+    res.status(404).json({success: false, message: err.message})
+  }
+}
 
 module.exports = {
   SignUp,
